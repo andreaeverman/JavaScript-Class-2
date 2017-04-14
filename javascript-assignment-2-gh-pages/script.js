81f4f86da3353e956ded67e6c92eae26d6cc6543
Andrea Everman <andrea.everman@id.iit.edu>
2017-04-13 21:19:15 -0500

    I forgot to // out the directions I had pasted in to follow the colors and numbers

--- javascript-assignment-2-gh-pages/script.js
+++ javascript-assignment-2-gh-pages/script.js
@@ -2,25 +2,25 @@
 
 
 
-1. Write a [`for`](https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Statements/for) loop to add a click event listener on all divs with class `box`.
+//1. Write a [`for`](https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Statements/for) loop to add a click event listener on all divs with class `box`.
 
-1. When the user clicks any box, change that box's background color to one of the following colors.
+//1. When the user clicks any box, change that box's background color to one of the following colors.
 
-    - If number of boxes on the page is between **201 and 300**, inclusive, change the background color of the box to **'yellow'**.
+    //- If number of boxes on the page is between **201 and 300**, inclusive, change the background color of the box to **'yellow'**.
 
-    - If number of boxes on the page is between **301 and 400**, inclusive, change the background color of the box to **'green'**.
+    //- If number of boxes on the page is between **301 and 400**, inclusive, change the background color of the box to **'green'**.
 
-    - If number of boxes on the page is between **401 and 500**, inclusive, change the background color of the box to **'purple'**.
+    //- If number of boxes on the page is between **401 and 500**, inclusive, change the background color of the box to **'purple'**.
 
-    - If number of boxes on the page is between **501 and 600**, inclusive, change the background color of the box to **'orange'**.
+    //- If number of boxes on the page is between **501 and 600**, inclusive, change the background color of the box to **'orange'**.
 
-    - If number of boxes on the page is between **601 and 700**, inclusive, change the background color of the box to **'pink'**.
+    //- If number of boxes on the page is between **601 and 700**, inclusive, change the background color of the box to **'pink'**.
 
-    - If number of boxes on the page is between **701 and 800**, inclusive, change the background color of the box to **'black'**.
+    //- If number of boxes on the page is between **701 and 800**, inclusive, change the background color of the box to **'black'**.
 
-    - If number of boxes on the page is between **801 and 900**, inclusive, change the background color of the box to **'brown'**.
+    //- If number of boxes on the page is between **801 and 900**, inclusive, change the background color of the box to **'brown'**.
 
-    - Otherwise, change the background color of the box to **'blue'**.
+    //- Otherwise, change the background color of the box to **'blue'**.
 
 
 // here is a loop example
